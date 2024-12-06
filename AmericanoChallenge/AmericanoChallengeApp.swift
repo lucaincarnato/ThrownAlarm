e4130e8cfc22c3a7a6a68b1828d06ae15110f82b
Luca Maria Incarnato <lucaincarnato.03@gmail.com>
2024-12-06 22:09:33 +0100

    Initial Commit

---
+++ AmericanoChallenge/AmericanoChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmericanoChallengeApp.swift
+//  AmericanoChallenge
+//
+//  Created by Luca Maria Incarnato on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AmericanoChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
