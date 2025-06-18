c2f9e0bdc5eb7c6619c8b29bcb1085f44ac057c8
Luca Maria Incarnato <lucaincarnato.03@gmail.com>
2025-06-18 11:49:59 +0200

    Disclaimer removed

--- ThrownAlarm/Views/DashboardView.swift
+++ ThrownAlarm/Views/DashboardView.swift
@@ -57,11 +57,6 @@ struct DashboardView: View {
                 OnboardingView(firstLaunch: $firstLaunch)
                     .interactiveDismissDisabled()
             }
-            /*
-            .sheet(isPresented: $update) {
-                DisclaimerView(update: $update)
-            }
-            */
         }
     }
 }
@@ -242,38 +237,5 @@ private struct AlarmView: View{
         if backtrack.isEmpty {return false}
         if Calendar.current.isDate(Date.now, inSameDayAs: backtrack.last!.date) {return true}
         return false
-    }
-}
-
-private struct DisclaimerView: View {
-    @Binding var update: Bool
-    
-    var body: some View {
-        NavigationStack{
-            VStack{
-                Image(systemName: "exclamationmark.triangle.fill")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .foregroundStyle(.red)
-                Text("Heads up! Maintenance coming")
-                    .font(.largeTitle)
-                    .bold()
-                    .padding(20)
-                Text("We're doing some data maintenance to update and improve our database.\n\nAs part of this process, all current data in the app will be deleted.\n\nThanks for your understanding – and sorry for the inconvenience!")
-                    .font(.headline)
-                    .padding(.horizontal, 20)
-                Button() {
-                    update.toggle()
-                } label: {
-                    Text("Got it")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical, 8)
-                }
-                .buttonStyle(.borderedProminent)
-                .padding()
-            }
-        }
     }
 }
